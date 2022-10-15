51a2880a8ccd50ed668cf34318674a42fd112684
trippone trippini <visualtrippa@hotmail.com>
2022-10-15 18:31:43 +0200

    asd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,10 +32,6 @@ namespace MVC_TDPC.Controllers
         {
             return View();
         }
-        public IActionResult ButtonPage()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
