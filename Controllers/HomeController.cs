94115d2838330afb65fe77089921bd471e7ffa09
trippone trippini <visualtrippa@hotmail.com>
2022-10-15 18:27:13 +0200

    asd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -44,10 +44,6 @@ namespace MVC_TDPC.Controllers
         {
             return View();
         }
-        public IActionResult ButtonPage()
-        {
-            return View();
-        }
         public IActionResult Javascript()
         {
             return View();
